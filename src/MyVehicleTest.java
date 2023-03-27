4a3c809f40e3f5f47f1d60b32ddff038f5aea434
Ashish <ashishparmar2000@yahoo.com>
2023-03-27 13:41:38 -0500

    testing vehicle

---
+++ src/MyVehicleTest.java
@@ -0,0 +1,24 @@
+public class MyVehicleTest {
+    public static void main(String args[]){
+        MyVehicle car1 = new MyVehicle();
+        MyVehicle car2 = new MyVehicle();
+        MyVehicle car3 = new MyVehicle();
+        MyVehicle car4 = new MyVehicle();
+        MyVehicle car5 = new MyVehicle();
+        MyVehicle car6 = new MyVehicle();
+        MyVehicle car7 = new MyVehicle();
+        MyVehicle car8 = new MyVehicle();
+
+
+        System.out.println(car1.toString());
+        System.out.println(car2.toString());
+        System.out.println(car3.toString());
+        System.out.println(car4.toString());
+        System.out.println(car5.toString());
+        System.out.println(car6.toString());
+        System.out.println(car7.toString());
+        System.out.println(car8.toString());
+
+    
+    }
+}
